4e848d3d1550452753b2321c79ac0babf860680a
EdwinPoma <pomachicomaedwin@gmail.com>
2026-09-14 02:04:51 -0500

    Agregar archivos de proyecto.

---
+++ ApiRestPractica/Controllers/RoutesController.cs
@@ -0,0 +1,28 @@
+﻿using ApiRestPractica.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiRestPractica.Controllers
+{
+    public class RoutesController : ApiController
+    {
+        public List<Route> Get()
+        {
+            Route nuevaRuta = new Route
+            {
+                Id = 1,
+                Nombre = "201"
+            };
+
+            List<Route> rutas = new List<Route>();
+
+            rutas.Add(nuevaRuta);
+
+            return rutas;
+        }
+    }
+}
