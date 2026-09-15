a6d990a30f6a43ee6460fa858463bdc91dff3272
EdwinPoma <pomachicomaedwin@gmail.com>
2026-09-15 00:40:26 -0500

    Se agrego un endpoint de get con un parametro

--- ApiRestPractica/Controllers/RoutesController.cs
+++ ApiRestPractica/Controllers/RoutesController.cs
@@ -24,5 +24,78 @@ namespace ApiRestPractica.Controllers
 
             return rutas;
         }
+
+        //public Route Get(int id)
+        //{
+        //    List<Route> listaRutas = new List<Route>();
+
+        //    Route ruta1 = new Route
+        //    {
+        //        Id = 1,
+        //        Nombre = "201"
+        //    };
+
+        //    Route ruta2 = new Route
+        //    {
+        //        Id = 2,
+        //        Nombre = "202"
+        //    };
+
+        //    Route ruta3 = new Route
+        //    {
+        //        Id = 3,
+        //        Nombre = "203"
+        //    };
+
+        //    listaRutas.Add(ruta1);
+        //    listaRutas.Add(ruta2);
+        //    listaRutas.Add(ruta3);
+
+        //    Route rutaEncontrada=listaRutas.FirstOrDefault(ruta => ruta.Id == id);
+
+        //    return rutaEncontrada;  
+        //}
+
+
+        public IHttpActionResult Get(int id)
+        {
+            List<Route> listaRutas = new List<Route>();
+
+            Route ruta1 = new Route
+            {
+                Id = 1,
+                Nombre = "201"
+            };
+
+            Route ruta2 = new Route
+            {
+                Id = 2,
+                Nombre = "202"
+            };
+
+            Route ruta3 = new Route
+            {
+                Id = 3,
+                Nombre = "203"
+            };
+
+            listaRutas.Add(ruta1);
+            listaRutas.Add(ruta2);
+            listaRutas.Add(ruta3);
+
+            Route rutaEncontrada = listaRutas.FirstOrDefault(ruta => ruta.Id == id);
+
+
+            if (rutaEncontrada != null)
+            {
+                return Ok(rutaEncontrada);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            
+        }
     }
 }
